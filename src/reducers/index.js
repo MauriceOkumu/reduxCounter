07fb8a02e3f7a86f35fd6e3d3ca6d41b1dc06644
Robin Kuehn <kuehn.robin@gmail.com>
2016-12-22 10:59:17 -0800

    Remove all spanish counter stuff

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,7 @@
 import { combineReducers } from 'redux';
-import EnglishCurrentReducer from './reducer_English';
-import SpanishCurrentReducer from './reducer_Spanish';
 
 const rootReducer = combineReducers({
-  EnglishCurrentReducer,
-  SpanishCurrentReducer,
+
   
 });
 
