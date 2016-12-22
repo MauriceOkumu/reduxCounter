9d8d2f96dc43cb120502f0b811a64c976e6ddd53
Robin Kuehn <kuehn.robin@gmail.com>
2016-12-22 10:53:13 -0800

    only english

--- containers/EnglishCounter.js
+++ containers/EnglishCounter.js
@@ -1,27 +1,14 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { increaseEnglish, decreaseEnglish, random } from '../src/actions/index';
 
-class EnglishCounter extends Component{
+export default class EnglishCounter extends Component{
   render(){
     return(
       <div className="counter"> 
         <h2> English Counter </h2>
-        <div onClick={() => this.props.increaseEnglish()}> Increase </div>
-        <p onClick={() => this.props.decreaseEnglish()}> Decrease </p>
-        <p>{this.props.Englishcurrent}</p>
+        <div> Increase </div>
+        <p> Decrease </p>
+        <p></p>
       </div>
     )
   }
 }
-
-const mapStateToProps = state => {
-  return {
-    Englishcurrent: state.EnglishCurrentReducer
-  };
-} 
-
-const mapDispatchToProps = dispatch => bindActionCreators({ increaseEnglish, decreaseEnglish, random }, dispatch);
-
-export default connect(mapStateToProps, mapDispatchToProps)(EnglishCounter);
